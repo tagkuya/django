8cec2f14b72088144ca2fd5ddc9ae9f370a6e494
tagkuya <fortis931taku@gmail.com>
2018-12-29 14:39:19 +0900

    add postspage to adminpage

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+
+  tittle = models.CharField(max_length=100)
+  
+  published = models.DateTimeField()
+
+  image = models.ImageField(upload_to='media/')
+
+  body = models.TextField()
+
+
+
